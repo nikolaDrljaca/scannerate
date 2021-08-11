64d452133f24b6935f0d1b0fa060f424fc838513
drljacao96 <drljacao96@hotmail.com>
2021-08-11 13:18:08 +0200

    Add missing licences.

--- app/src/main/java/com/drbrosdev/studytextscan/ui/licences/LicenceViewModel.kt
+++ app/src/main/java/com/drbrosdev/studytextscan/ui/licences/LicenceViewModel.kt
@@ -16,7 +16,10 @@ class LicenceViewModel() : ViewModel() {
         allLicenceItems = listOf(
             LicenceListItem.KoinLicence,
             LicenceListItem.LottieLicence,
-            LicenceListItem.EpoxyLicence
+            LicenceListItem.EpoxyLicence,
+            LicenceListItem.OnBoardingAnim2Screen,
+            LicenceListItem.OnBoardingAnim3Screen,
+            LicenceListItem.AppIcon
         )
     }
 
@@ -28,6 +31,8 @@ class LicenceViewModel() : ViewModel() {
         object KoinLicence : LicenceListItem(1,"Koin", "https://insert-koin.io/")
         object LottieLicence : LicenceListItem(2,"Lottie", "https://github.com/airbnb/lottie-android")
         object EpoxyLicence: LicenceListItem(3,"Epoxy", "https://github.com/airbnb/epoxy")
-
+        object OnBoardingAnim2Screen : LicenceListItem(4, "Animation 1", "https://lottiefiles.com/57966-edit-animation")
+        object OnBoardingAnim3Screen : LicenceListItem(5,"Animation 2", "https://lottiefiles.com/47226-pdf-file")
+        object AppIcon : LicenceListItem(6, "App Icon", "https://www.flaticon.com/free-icon/text-recognising_3997552?related_id=3997610&origin=search")
     }
 }
