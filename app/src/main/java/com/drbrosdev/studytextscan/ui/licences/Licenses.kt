ba0599523c5700aba9d2bfceb66d6d37a21b079e
nikolaDrljaca <devnikoladr@gmail.com>
2023-01-07 19:20:09 +0300

    Migrated licenses screen to compose.

---
+++ app/src/main/java/com/drbrosdev/studytextscan/ui/licences/Licenses.kt
@@ -0,0 +1,38 @@
+package com.drbrosdev.studytextscan.ui.licences
+
+val allLicenceItems = listOf(
+    LicenceListItem.KoinLicence,
+    LicenceListItem.LottieLicence,
+    LicenceListItem.EpoxyLicence,
+    LicenceListItem.OnBoardingAnim2Screen,
+    LicenceListItem.OnBoardingAnim3Screen,
+    LicenceListItem.AppIcon,
+    LicenceListItem.PermissionInfoAnim
+)
+
+sealed class LicenceListItem(
+    val order: Int,
+    val title: String,
+    val link: String
+) {
+    object KoinLicence : LicenceListItem(1, "Koin", "https://insert-koin.io/")
+    object LottieLicence : LicenceListItem(2, "Lottie", "https://github.com/airbnb/lottie-android")
+    object EpoxyLicence : LicenceListItem(3, "Epoxy", "https://github.com/airbnb/epoxy")
+    object OnBoardingAnim2Screen :
+        LicenceListItem(4, "Animation 1", "https://lottiefiles.com/57966-edit-animation")
+
+    object OnBoardingAnim3Screen :
+        LicenceListItem(5, "Animation 2", "https://lottiefiles.com/47226-pdf-file")
+
+    object AppIcon : LicenceListItem(
+        6,
+        "App Icon",
+        "https://www.flaticon.com/free-icon/text-recognising_3997552?related_id=3997610&origin=search"
+    )
+
+    object PermissionInfoAnim : LicenceListItem(
+        order = 7,
+        "Animation 3",
+        "https://lottiefiles.com/96057-tta-information-session"
+    )
+}
---
+++ app/src/main/java/com/drbrosdev/studytextscan/ui/licences/ScannerateLicensesScreen.kt
@@ -0,0 +1,63 @@
+package com.drbrosdev.studytextscan.ui.licences
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.drbrosdev.studytextscan.ui.support.theme.BackgroundBlue
+
+@Composable
+fun ScannerateLicensesScreen(
+    modifier: Modifier = Modifier,
+    onLicenseClick: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier.then(modifier),
+        color = MaterialTheme.colors.background
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(allLicenceItems) { license ->
+                LicenseCard(
+                    license = license,
+                    onClick = { onLicenseClick(license.link) }
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun LicenseCard(
+    modifier: Modifier = Modifier,
+    license: LicenceListItem,
+    onClick: () -> Unit
+) {
+    Card(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .then(modifier),
+        shape = RoundedCornerShape(16.dp),
+        elevation = 0.dp,
+        backgroundColor = MaterialTheme.colors.primary
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp)
+        ) {
+            Text(text = license.title, fontSize = 16.sp)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = license.link, color = BackgroundBlue, fontSize = 12.sp)
+        }
+    }
+}
